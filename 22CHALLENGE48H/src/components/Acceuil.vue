929280f480d1ae4cf300080cad7b1553431f3e37
Flavio <flavio.cormerais@ynov.com>
2022-02-07 18:11:30 +0100

    page d'acceuil final

--- 22CHALLENGE48H/src/components/Acceuil.vue
+++ 22CHALLENGE48H/src/components/Acceuil.vue
@@ -12,43 +12,46 @@
     <div class="presentationNous">
         <div class="description">
         <img class="photo" src="https://static.vecteezy.com/ti/vecteur-libre/p1/1840612-image-profil-icon-male-icon-human-or-people-sign-and-symbol-vector-gratuit-vectoriel.jpg"/>
-        <h1>Maitre Jedi Damien LANDOIS DEV B3</h1>
+        <h3>Maitre Jedi Damien LANDOIS DEV B3</h3>
     </div>
     <div class="description">
         <img class="photo" src="https://static.vecteezy.com/ti/vecteur-libre/p1/1840612-image-profil-icon-male-icon-human-or-people-sign-and-symbol-vector-gratuit-vectoriel.jpg"/>
-        <h1>Padawan Jules OUTIN DEV B1</h1>
+        <h3>Padawan Jules OUTIN DEV B1</h3>
     </div>
     <div class="description">
         <img class="photo" src="https://static.vecteezy.com/ti/vecteur-libre/p1/1840612-image-profil-icon-male-icon-human-or-people-sign-and-symbol-vector-gratuit-vectoriel.jpg"/>
-        <h1>Chevalier Jedi Elouan DUMONT DEV B2</h1>
+        <h3>Chevalier Jedi Elouan DUMONT DEV B2</h3>
     </div>
     <div class="description">
         <img class="photo" src="https://static.vecteezy.com/ti/vecteur-libre/p1/1840612-image-profil-icon-male-icon-human-or-people-sign-and-symbol-vector-gratuit-vectoriel.jpg"/>
-        <h1>Padawan Abakar mele TIDJANI DEV B1</h1>
+        <h3>Padawan Abakar mele TIDJANI DEV B1</h3>
     </div>
     <div class="description">
         <img class="photo" src="https://static.vecteezy.com/ti/vecteur-libre/p1/1840612-image-profil-icon-male-icon-human-or-people-sign-and-symbol-vector-gratuit-vectoriel.jpg"/>
-        <h1>Chevalier Jedi Tao BOURMAUD DEV B2</h1>
+        <h3>Chevalier Jedi Tao BOURMAUD DEV B2</h3>
     </div>  
     <div class="description">
         <img class="photo" src="https://static.vecteezy.com/ti/vecteur-libre/p1/1840612-image-profil-icon-male-icon-human-or-people-sign-and-symbol-vector-gratuit-vectoriel.jpg"/>
-        <h1>Padawan Flavio CORMERAIS DEV B1</h1>
+        <h3>Padawan Flavio CORMERAIS DEV B1</h3>
     </div>
     </div>
     <div class="texteExpliquation">
-        <h1>hellosmfgjsljhglkd jhlkfdghhlkfdhhlkjdskghsdfkhg dskfhgksfdhglkfdshklgfjh</h1>
+        <h1>Nous vous présentons notre projet qui a pour but de creer un jeu ludique avec l'univers de star wars en utilisant une api (swapi). Ce projet est a été proposé dans le cadre d'un projet 48 h par l'école ynov 2022</h1>
     </div>
 </div> 
 </template>
 
 <style scoped>
 .texteExpliquation{
-    margin: 0 5% 0 0;
-    padding: 15px;
+    grid-row: 2;
+    grid-column: 3;
     border: 3px solid #ffe919;
+    color :white;
+    text-align:center;
 }
 .description{
     margin: 2% 2% 2% 2%;
+    color : white;
 }
 .navbar{
 	display: flex;
@@ -63,18 +66,23 @@
     width: auto;
 }
 .presentationNous{
+    grid-column: 1/3;
+    grid-row: 2 /4;
 	display: flex;
 	flex-wrap: wrap;
 	justify-content: center;
 	align-items: stretch;
 	align-content: stretch;
     border: 3px solid #ffe919;
-    margin: 0 5% 0 5%;
 }
 .corps
     {
     margin: 3% 0 0 0;
-	display: flex;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 0px 300px;
+    grid-gap: 10px;
+
 }
 .myButton {
 color: rgb(0, 0, 0);
@@ -86,11 +94,13 @@ color: rgb(0, 0, 0);
     box-shadow: rgb(0, 0, 0) 5px 5px 15px 5px;
     border: 2px solid rgb(245, 255, 21); 
     display: inline-block;
-    font-family:"Bebas Neue";}
+    font-family:"Bebas Neue"
+    }
 .myButton:hover {
 background: rgb(245, 255, 21) }
 .myButton:active {
-background:rgb(200, 207, 63) }
+background:rgb(200, 207, 63) 
+}
 
 .photo{
     height: 200px;
