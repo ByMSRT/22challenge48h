106325787dc7443f16612171465f4deecd49bcf1
Damien <landois.damien@live.fr>
2022-02-07 11:29:14 +0100

    add components and router api

---
+++ 22CHALLENGE48H/src/api/route.js
@@ -0,0 +1,13 @@
+import api from 'axios'
+
+export default{ 
+    async apiGetPlanets(){
+        return await api.get("https://swapi.dev/api/",
+        {
+            headers:{
+            "Content-Type": 'application/json' 
+            }
+        });
+    }
+    Planets, Spaceships, Vehicles, People, Films and Species
+}
