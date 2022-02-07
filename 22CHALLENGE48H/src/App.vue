5107c0babe38a47b1a837fc5f52d6eb04ad567f1
Flavio <flavio.cormerais@ynov.com>
2022-02-07 16:24:01 +0100

    push de la page d'acceuil

--- 22CHALLENGE48H/src/App.vue
+++ 22CHALLENGE48H/src/App.vue
@@ -1,21 +1,29 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/Acceuil.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+<body>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
   <HelloWorld msg="Hello Vue 3 + Vite" />
+  </body>
 </template>
 
 <style>
+body{
+  background-image:url(https://tetesenlair.net/heberg_tetesenlair/PierreL/2017_11_26_ngc-457-ps-rotah-90-01.jpg);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  font-family:"Bebas Neue";
   margin-top: 60px;
 }
 </style>
--- 22CHALLENGE48H/src/components/Acceuil.vue
+++ 22CHALLENGE48H/src/components/Acceuil.vue
@@ -3,9 +3,97 @@
 </script>
 
 <template>
-
+<div class="navbar">
+    <button class="myButton"> WIKI INFO </button>
+    <img class="bonneTaille" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/2560px-Star_Wars_Logo.svg.png" />
+    <button class="myButton">JEU QUI SUIS-JE ??</button>
+</div>
+<div class="corps">
+    <div class="presentationNous">
+        <div class="description">
+        <img class="photo" src="https://static.vecteezy.com/ti/vecteur-libre/p1/1840612-image-profil-icon-male-icon-human-or-people-sign-and-symbol-vector-gratuit-vectoriel.jpg"/>
+        <h1>Flavio Cormerais DEV B1</h1>
+    </div>
+    <div class="description">
+        <img class="photo" src="https://static.vecteezy.com/ti/vecteur-libre/p1/1840612-image-profil-icon-male-icon-human-or-people-sign-and-symbol-vector-gratuit-vectoriel.jpg"/>
+        <h1>Flavio Cormerais DEV B1</h1>
+    </div>  
+    <div class="description">
+        <img class="photo" src="https://static.vecteezy.com/ti/vecteur-libre/p1/1840612-image-profil-icon-male-icon-human-or-people-sign-and-symbol-vector-gratuit-vectoriel.jpg"/>
+        <h1>Flavio Cormerais DEV B1</h1>
+    </div>
+    <div class="description">
+        <img class="photo" src="https://static.vecteezy.com/ti/vecteur-libre/p1/1840612-image-profil-icon-male-icon-human-or-people-sign-and-symbol-vector-gratuit-vectoriel.jpg"/>
+        <h1>Flavio Cormerais DEV B1</h1>
+    </div>
+    <div class="description">
+        <img class="photo" src="https://static.vecteezy.com/ti/vecteur-libre/p1/1840612-image-profil-icon-male-icon-human-or-people-sign-and-symbol-vector-gratuit-vectoriel.jpg"/>
+        <h1>Flavio Cormerais DEV B1</h1>
+    </div>
+    <div class="description">
+        <img class="photo" src="https://static.vecteezy.com/ti/vecteur-libre/p1/1840612-image-profil-icon-male-icon-human-or-people-sign-and-symbol-vector-gratuit-vectoriel.jpg"/>
+        <h1>Flavio Cormerais DEV B1</h1>
+    </div>
+    </div>
+    <div class="texteExpliquation">
+        <h1>hellosmfgjsljhglkd jhlkfdghhlkfdhhlkjdskghsdfkhg dskfhgksfdhglkfdshklgfjh</h1>
+    </div>
+</div> 
 </template>
 
 <style scoped>
-    
+.texteExpliquation{
+    margin: 0 10% 0 0;
+    padding: 15px;
+    border: 3px solid #ffe919;
+}
+.description{
+    margin: 2% 2% 2% 2%;
+}
+.navbar{
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	justify-content: space-around;
+	align-items: flex-start;
+	align-content: flex-start;
+}
+.bonneTaille{
+    height: 250px;
+    width: auto;
+}
+.presentationNous{
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	align-items: stretch;
+	align-content: stretch;
+    border: 3px solid #ffe919;
+    margin: 0 10% 0 0;
+}
+.corps
+    {
+    margin: 7% 0 0 0;
+	display: flex;
+}
+.myButton {
+color: rgb(0, 0, 0);
+    font-size: 29px;
+    line-height: 29px;
+    padding: 20px;
+    border-radius: 27px;
+    background-color: #ffe919;
+    box-shadow: rgb(0, 0, 0) 5px 5px 15px 5px;
+    border: 2px solid rgb(245, 255, 21); 
+    display: inline-block;}
+.myButton:hover {
+background: rgb(245, 255, 21) }
+.myButton:active {
+background:rgb(200, 207, 63) }
+
+.photo{
+    height: 200px;
+    width: auto;
+    margin : 2% 2% 2% 2%;
+}
 </style>
