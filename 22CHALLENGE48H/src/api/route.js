6db9afee554d99668184117cf48404c9f5706671
Damien <landois.damien@live.fr>
2022-02-07 14:46:59 +0100

    add axios, && list planet wiki

--- 22CHALLENGE48H/src/api/route.js
+++ 22CHALLENGE48H/src/api/route.js
@@ -2,12 +2,99 @@ import api from 'axios'
 
 export default{ 
     async apiGetPlanets(){
-        return await api.get("https://swapi.dev/api/",
+        return await api.get("https://swapi.dev/api/planets",
+        {
+            headers:{
+            "Content-Type": 'application/json' 
+            }
+        });
+    },
+    async apiGetPlanetsById(id){
+        return await api.get(`https://swapi.dev/api/planets/${id}`,
+        {
+            headers:{
+            "Content-Type": 'application/json' 
+            }
+        });
+    },
+    async apiGetSpaceships(){
+        return await api.get("https://swapi.dev/api/spaceships",
+        {
+            headers:{
+            "Content-Type": 'application/json' 
+            }
+        });
+    },
+    async apiGetSpaceshipsById(id){
+        return await api.get(`https://swapi.dev/api/spaceships/${id}`,
+        {
+            headers:{
+            "Content-Type": 'application/json' 
+            }
+        });
+    },
+    async apiGetVehicles(){
+        return await api.get("https://swapi.dev/api/vehicles",
+        {
+            headers:{
+            "Content-Type": 'application/json' 
+            }
+        });
+    },
+    async apiGetVehiclesById(id){
+        return await api.get(`https://swapi.dev/api/vehicles/${id}`,
+        {
+            headers:{
+            "Content-Type": 'application/json' 
+            }
+        });
+    },
+    async apiGetPeople(){
+        return await api.get("https://swapi.dev/api/people",
+        {
+            headers:{
+            "Content-Type": 'application/json' 
+            }
+        });
+    },
+    async apiGetPeopleById(id){
+        return await api.get(`https://swapi.dev/api/people/${id}`,
+        {
+            headers:{
+            "Content-Type": 'application/json' 
+            }
+        });
+    },
+    async apiGetFilms(){
+        return await api.get("https://swapi.dev/api/films",
+        {
+            headers:{
+            "Content-Type": 'application/json' 
+            }
+        });
+    },
+    async apiGetFilmsById(id){
+        return await api.get(`https://swapi.dev/api/films/${id}`,
+        {
+            headers:{
+            "Content-Type": 'application/json' 
+            }
+        });
+    },
+    async apiGetSpecies(){
+        return await api.get("https://swapi.dev/api/species",
+        {
+            headers:{
+            "Content-Type": 'application/json' 
+            }
+        });
+    },
+    async apiGetSpeciesById(id){
+        return await api.get(`https://swapi.dev/api/species/${id}`,
         {
             headers:{
             "Content-Type": 'application/json' 
             }
         });
     }
-    Planets, Spaceships, Vehicles, People, Films and Species
 }
--- 22CHALLENGE48H/src/components/Wiki.vue
+++ 22CHALLENGE48H/src/components/Wiki.vue
@@ -1,9 +1,19 @@
-<script>
+<script setup>
+import API from '../api/route.js';
+import {ref} from 'vue';
 
+const listPlanets= ref([]);
+
+async function callPlanets(){
+    console.log('callapi')
+    listPlanets.value = (await API.apiGetPlanets()).data.results;
+    console.log(listPlanets.value)
+}
 </script>
 
 <template>
-
+    <input type="submit" name="submit" value="planets" v-on:click="callPlanets">
+    <article v-for="planet of listPlanets">{{ planet.name}} {{planet.diameter}}</article>
 </template>
 
 <style scoped>
