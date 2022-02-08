a846d7be8787abb2d39ba6feb88782b0edc14440
Damien <landois.damien@live.fr>
2022-02-08 22:23:50 +0100

    update routing and creat header components

--- 22CHALLENGE48H/src/App.vue
+++ 22CHALLENGE48H/src/App.vue
@@ -2,15 +2,18 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import {RouterLink, RouterView} from"vue-router";
+import Header from './components/Header.vue';
 </script>
 
 <template>
-  <RouterView>
-      
-    </RouterView>
+  <Header/>
+  <RouterView/>
 </template>
 
 <style>
+body {
+  background-image: url(https://ak.picdn.net/shutterstock/videos/1021959073/thumb/1.jpg?ip=x480);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
