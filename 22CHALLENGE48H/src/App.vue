16a5c071cd4892d4fcd00c7e9eb3f91787ec8bd0
ATIDJANI <abakarmele.tidjani@ynov.com>
2022-02-08 14:22:25 +0100

    QuiSuis.vue

--- 22CHALLENGE48H/src/App.vue
+++ 22CHALLENGE48H/src/App.vue
@@ -2,10 +2,12 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import Wiki from './components/Wiki.vue'
+import Game from './components/QuiSuisJe.vue'
 </script>
 
 <template>
   <Wiki></Wiki>
+  <Game></Game>
 </template>
 
 <style>
--- 22CHALLENGE48H/src/components/QuiSuisJe.vue
+++ 22CHALLENGE48H/src/components/QuiSuisJe.vue
@@ -3,9 +3,84 @@
 </script>
 
 <template>
+<nav>
 
+    <button class="home"><a href="https://starwars-visualguide.com/#/">Home</a></button>
+
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/2560px-Star_Wars_Logo.svg.png" />
+
+    <button class="Wiki"><a href="https://kennethscoggins.medium.com/how-to-use-the-star-wars-api-in-about-50-lines-of-php-b78b39e9eb8e">Wiki</a></button>
+
+</nav>
 </template>
 
 <style scoped>
+nav {
 
+    display: flex;
+
+    flex-direction: row;
+
+    flex-wrap: wrap;
+
+    justify-content: space-around;
+
+    align-items: center;
+
+    align-content: stretch;
+
+    margin-bottom: 50px;
+
+}
+
+button {
+
+    border: 0;
+
+    border-radius: 60px;
+
+    width: 120px;
+
+    height: 50px;
+
+   
+
+}
+
+button:hover {
+
+    background-color: #EBE501;
+
+}
+
+.home {
+
+    background-color: yellow;
+
+}
+
+.wiki {
+
+    background-color: yellow;
+
+}
+a {
+
+    text-decoration: none;
+
+    color: black;
+
+    font-family: "Bebas Neue";
+
+    font-size: 25px;
+
+}
+
+img {
+
+    width: 350px;
+
+    height: auto;
+
+}
 </style>
